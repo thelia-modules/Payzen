3cb62ebac6929da85b57dafbce9bc1ecd0213f1c
apenalver <apenalver@openstudio.fr>
2015-07-24 11:57:52 +0200

    fix email templating fix email init

---
+++ I18n/email/default/en_US.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    "The payment of your order is confirmed at %link" => "The payment of your order is confirmed at %link",
+    "The payment of your order is confirmed" => "The payment of your order is confirmed",
+    "Your invoice is now available in your customer account on" => "Your invoice is now available in your customer account on",
+    "Thank you again for your purchase." => "Thank you again for your purchase.",
+    "The %name team" => "The %name team.",
+    "Dear customer," => "Dear customer,",
+    "This is a confirmation of the payment of your order %ref via Payzen on our shop." => "This is a confirmation of the payment of your order %ref via Payzen on our shop.",
+    "Your invoice is now available in your customer account at %link" => "Your invoice is now available in your customer account at %link",
+    'Order payment confirmation' => 'Order payment confirmation',
+    'Order {$order_ref} payment confirmation' => 'Order {$order_ref} payment confirmation'
+);
